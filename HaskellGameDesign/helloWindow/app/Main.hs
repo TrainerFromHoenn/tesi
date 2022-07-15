2043257b5de1136ffdbdac0a1bfc128e24963a89
TrainerFromHoenn <michelebiena@gmail.com>
2022-07-15 14:01:37 +0200

    Completamento Shapes

--- HaskellGameDesign/helloWindow/app/Main.hs
+++ HaskellGameDesign/helloWindow/app/Main.hs
@@ -1,19 +1,17 @@
 module Main where
--- base 
-import Control.Monad (when)
+
+-- base
+
 import Control.Exception (bracket)
-
--- GLFW-b, qualificato
-import qualified Graphics.UI.GLFW as GLFW
-
+import Control.Monad (when)
 -- gl in generale
 import Graphics.GL.Core33
 import Graphics.GL.Types
-
+import qualified Graphics.UI.GLFW as GLFW
 import Lib
 
-
 minWidth = 800
+
 minHeight = 600
 
 winTitle = "Hello Window"
@@ -21,44 +19,44 @@ winTitle = "Hello Window"
 -- ora definisco il callback per ogni volta che premo un tasto
 -- in prima battuta stampiamo solo il tasto premuto
 callback :: GLFW.KeyCallback
-callback window key scanCode keyState modKeys = do 
-    print key 
-    when (key == GLFW.Key'Escape && keyState == GLFW.KeyState'Pressed)
-        (GLFW.setWindowShouldClose window True)
+callback window key scanCode keyState modKeys = do
+  print key
+  when
+    (key == GLFW.Key'Escape && keyState == GLFW.KeyState'Pressed)
+    (GLFW.setWindowShouldClose window True)
 
 -- nel main invece inizializziamo GLFW, con i nostri suggerimenti e poi renderizziamo uno schermo nero
 
 main :: IO ()
 main = bracketGLFW $ do
-    GLFW.windowHint (GLFW.WindowHint'ContextVersionMajor 3)
-    GLFW.windowHint (GLFW.WindowHint'ContextVersionMinor 3)
-    GLFW.windowHint (GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core)
-    GLFW.windowHint (GLFW.WindowHint'Resizable False)
-    maybeWindow <- GLFW.createWindow minWidth minHeight winTitle Nothing Nothing
-    case maybeWindow of
-        Nothing -> putStrLn "Failed to create a GLFW window!"
-        Just window -> do
-            -- enable keys
-            GLFW.setKeyCallback window (Just callback)
-            -- calibrate the viewport
-            GLFW.makeContextCurrent (Just window)
-            (x,y) <- GLFW.getFramebufferSize window
-            glViewport 0 0 (fromIntegral x) (fromIntegral y)
-            -- enter our main loop
-            let loop = do
-                    shouldContinue <- not <$> GLFW.windowShouldClose window
-                    when shouldContinue $ do
-                        -- event poll
-                        GLFW.pollEvents
-                        -- drawing
-                        glClearColor 0.2 0.3 0.3 1.0
-                        glClear GL_COLOR_BUFFER_BIT
-                        -- swap buffers and go again
-                        GLFW.swapBuffers window
-                        loop
-            loop
-
+  GLFW.windowHint (GLFW.WindowHint'ContextVersionMajor 3)
+  GLFW.windowHint (GLFW.WindowHint'ContextVersionMinor 3)
+  GLFW.windowHint (GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core)
+  GLFW.windowHint (GLFW.WindowHint'Resizable False)
+  maybeWindow <- GLFW.createWindow minWidth minHeight winTitle Nothing Nothing
+  case maybeWindow of
+    Nothing -> putStrLn "Failed to create a GLFW window!"
+    Just window -> do
+      -- enable keys
+      GLFW.setKeyCallback window (Just callback)
+      -- calibrate the viewport
+      GLFW.makeContextCurrent (Just window)
+      (x, y) <- GLFW.getFramebufferSize window
+      glViewport 0 0 (fromIntegral x) (fromIntegral y)
+      -- enter our main loop
+      let loop = do
+            shouldContinue <- not <$> GLFW.windowShouldClose window
+            when shouldContinue $ do
+              -- event poll
+              GLFW.pollEvents
+              -- drawing
+              glClearColor 0.2 0.3 0.3 1.0
+              glClear GL_COLOR_BUFFER_BIT
+              -- swap buffers and go again
+              GLFW.swapBuffers window
+              loop
+      loop
 
 bracketGLFW :: IO () -> IO ()
-bracketGLFW act = bracket GLFW.init (const GLFW.terminate) $ \initWorked -> 
-    when initWorked act
+bracketGLFW act = bracket GLFW.init (const GLFW.terminate) $ \initWorked ->
+  when initWorked act
--- HaskellGameDesign/shapes/app/Main.hs
+++ HaskellGameDesign/shapes/app/Main.hs
@@ -1,58 +1,100 @@
-module Main where
+{-# LANGUAGE PackageImports #-}
 
-import Lib
-import "GLFW-b" Graphics.Ui.GLFW as GLFW
+import Control.Concurrent (threadDelay)
+import Control.Monad (Functor (fmap), Monad (return), unless, when)
+import Data.Bool (Bool (True))
+import Data.List (unwords)
+import Data.Maybe (Maybe (Just, Nothing))
+import Data.String (String)
+import Graphics.Gloss (Picture (Color, Pictures), arcSolid, bright, chartreuse, circle, circleSolid, cyan, dark, dim, green, light, line, lineLoop, magenta, makeColor, polygon, rectanglePath, rectangleSolid, rectangleWire, red, rotate, scale, text, thickArc, thickCircle, translate, violet, white, yellow)
+import Graphics.Gloss.Interface.IO.Game (Key, KeyState)
+import Graphics.Gloss.Rendering (displayPicture, initState)
+import Graphics.UI.GLFW (Key (Key'Escape), KeyState (KeyState'Pressed, KeyState'Repeating), Window, pollEvents, swapBuffers, windowHint)
+import qualified Graphics.UI.GLFW as GLFW
+import "GLFW-b" Graphics.UI.GLFW as GLFW
+import System.Exit (exitSuccess)
 
--- Funzione che permette la gestione della finestra, non serve ricordarla
-withWindow :: Int -> Int -> String -> (GLFW.Window -> IO ()) -> IO ()
+windowWidth, windowHeight :: Int
+windowWidth = 640
+windowHeight = 480
+
+-- Funzione conveniente per la creazione di finestre
+withWindow :: Int -> Int -> String -> (Window -> IO ()) -> IO ()
 withWindow width height title f = do
-    GLFW.setErrorCallback $ Just simpleErrorCallback
-    r <- GLFW.init
-    when r $ do
-        m <- GLFW.createWindow width height title Nothing Nothing
-        case m of
-            (Just win) -> do
-                GLFW.makeContextCurrent m
-                f win
-                GLFW.setErrorCallback $ Just simpleErrorCallback
-                GLFW.destroyWindow win
-            Nothing -> return ()
-        GLFW.terminate
-    where
-        simpleErrorCallback e s =
-            putStrLn $ unwords [show e, show s]
+  setErrorCallback $ Just simpleErrorCallback
+  r <- GLFW.init
+  when r $ do
+    m <- GLFW.createWindow width height title Nothing Nothing
+    case m of
+      (Just win) -> do
+        GLFW.makeContextCurrent m
+        f win
+        GLFW.setErrorCallback $ Just simpleErrorCallback
+        GLFW.destroyWindow win
+      Nothing -> return ()
+    GLFW.terminate
+  where
+    simpleErrorCallback e s =
+      putStrLn $ unwords [show e, show s]
 
-main :: IO ()
-main = do
-    let width = 640
-        height = 480
-    withWindow width height "Resurrection" $ \win -> do 
-        loop win
+-- Con la funzione withWindow, il main è stupidissimo
+-- main :: IO ()
+-- main = do
+--
 
--- Implementazione di un loop
-loop window = do 
-    pollEvents -- cerca input
-    renderFrame window -- renderizza il prossimo frame (definizione sotto)
-    threadDelay 2000 -- Do il tempo a OpenGL di renderizzare completamente il frame (2000 microsecondi)
-    -- La questione fps costanti è roba da malati ed è inutile fregarmene ora
-    k <- keyIsPressed window Key'Escape -- possibile uscita dal loop (definizione di keyIsPressed sotto)
-    if k 
-        then return ()
-        else loop window
+-- Creiamo il loop del nostro programma
+loop window = do
+  pollEvents -- Controlla gli input utente
+  renderFrame window -- Renderizza il frame
+  threadDelay 20000 -- Permette al sistema di avere il tempo di renderizzare (circa 60fps)
+  k <- keyIsPressed window Key'Escape -- Possibile uscita dal loop
+  if k
+    then return ()
+    else loop window
 
-keyIsPressed :: Window -> Key -> IO Bool
+keyIsPressed :: Window -> GLFW.Key -> IO Bool -- Funzione per controllare il tasto premuto
 keyIsPressed win key = isPress `fmap` GLFW.getKey win key
 
-isPress :: KeyState -> Bool
+isPress :: GLFW.KeyState -> Bool
 isPress KeyState'Pressed = True
 isPress KeyState'Repeating = True
 isPress _ = False
 
--- Per disegnare si usa la funzione displayPicture di Gloss
--- Input: larghezza/altezza finestra, colore, rendering state, view port, disegno vero e proprio
--- Output: azioni IO
-
 renderFrame window = do
-    -- qua si disegna
-    swapBuffers window
+  -- Funzione per renderizzare
+  -- Disegni di vario tipo
+  displayPicture (width, height) white glossState 1.0 $
+    Pictures
+      [ Color violet $
+          translate (-300) 100 $
+            polygon [(-10, 10), (-10, 70), (20, 20), (20, 30)], -- Poligono viola
+        Color red $
+          translate (-200) 100 $
+            line [(-30, 30), (-40, 30), (30, 40), (50, -20)], -- linea rossa a segmenti
+        Color (makeColor 0 128 255 1) $
+          translate (-200) 100 $
+            lineLoop [(-30, -30), (-40, 30), (30, 40), (50, -20)], -- linea azzurrina a segmenti che si richiude
+        Color red $
+          translate 100 100 $
+            circle 30, -- cerchio rosso
+        Color green $
+          translate 100 100 $
+            thickCircle 30 10, -- cerchio con linea spessa verde
+        Color yellow $
+          translate 200 100 $
+            circleSolid 30, -- cerchio pieno giallo
+        Color chartreuse $
+          translate (-200) (-100) $
+            thickArc 0 180 30 30, -- arco verdognolo
+        Color (dark magenta) $
+          translate (-100) (-100) $
+            arcSolid 0 90 30, -- arco pieno magenta
+        Color (bright magenta) $
+          translate 0 (-100) $
+            scale 0.2 0.2 $
+              text "Boo!", -- testo piccolo magenta
+        Color (dim cyan) $ translate 100 (-100) $ rotate 30 $ rectangleWire 20 50,
+        Color (light cyan) $ translate 200 (-100) $ rotate 60 $ rectangleSolid 20 50 -- rettangolo pieno ciano
+      ]
 
+  swapBuffers window -- Scambia frame vecchio con frame nuovo
